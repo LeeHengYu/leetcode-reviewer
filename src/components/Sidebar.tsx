feeb7064840332ba7b9ec7860c97a2845907d9be
hengyulee <hengyuli901229@gmail.com>
2023-06-16 20:48:55 +0800

    Move the links to a single files

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,21 +1,20 @@
-import { Heading, Link, VStack } from "@chakra-ui/react";
+import { Box, Heading, Link, VStack } from "@chakra-ui/react";
+import links from "../data/links";
 
 const Sidebar = () => {
   return (
     <VStack paddingY={2}>
-      <Heading size="md">Other Links</Heading>
-      <Link href="#" marginY={2}>
-        Link
-      </Link>
-      <Link href="#" marginY={2}>
-        Link
-      </Link>
-      <Link href="#" marginY={2}>
-        Link
-      </Link>
-      <Link href="#" marginY={2}>
-        Link
-      </Link>
+      <Heading size="md" marginBottom={2}>
+        Other Links
+      </Heading>
+      {links.map((data) => (
+        <Box marginY={2}>
+          <Link href={data.url} target="_blank">
+            {data.text}
+          </Link>
+        </Box>
+      ))}
+      ;
     </VStack>
   );
 };
