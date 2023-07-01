4c58328d559690b1fa2e922280cc1cb700d83023
hengyulee <hengyuli901229@gmail.com>
2023-07-02 00:38:37 +0800

    remove podcast links

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,7 +1,6 @@
 const links = [
     {text: "Linkedin", url: "https://www.linkedin.com/in/heng-yu-lee-4b179420a/"},
-    {text: "Podcast", url: "https://open.spotify.com/show/3WHSDto77XxYbyg4LhhQ3K?si=851d67cea6774651"},
-    {text: "Instagram", url: "https://www.instagram.com/kh_grindingrealhard/"},
+    {text: "Instagram", url: "https://www.instagram.com/khhk_universitydegenerate/"},
     {text: "Linktree", url: "https://linktr.ee/hengyulee"},
     {text: "Contact Me", url: "mailto:hengyuli901229@gmail.com"}
 ];
