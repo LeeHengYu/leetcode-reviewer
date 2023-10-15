0678a4ff1669312e6b3dd8a30254824d24e0faf6
hengyulee <hengyuli901229@gmail.com>
2023-10-15 09:29:38 +0800

    add

--- src/data/tablaData.ts
+++ src/data/tablaData.ts
@@ -1649,6 +1649,14 @@ const tableData: Question[] = [
       solution: "min-cost-climbing-stairs.py",
       dailyChallenge: new Date("2023-10-13"),
     },
+    {
+      question: "Number of Ways to Stay in the Same Place After Some Steps",
+      link: "https://leetcode.com/problems/number-of-ways-to-stay-in-the-same-place-after-some-steps/",
+      category: "Dynamic Programming",
+      difficulty: "Hard",
+      solution: "number-of-ways-to-stay-in-the-same-place-after-some-steps.py",
+      dailyChallenge: new Date("2023-10-15"),
+    },
   ];
 
 export default tableData;
