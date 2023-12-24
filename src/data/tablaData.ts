18967a474432cc4fa9315cd8202edfe13a711956
hengyulee <hengyuli901229@gmail.com>
2023-12-24 10:36:13 +0800

    add 1224

--- src/data/tablaData.ts
+++ src/data/tablaData.ts
@@ -2209,6 +2209,14 @@ const tableData: Question[] = [
       solution: "path-crossing.py",
       dailyChallenge: new Date("2023-12-23"),
     },
+    {
+      question: "Minimum Changes To Make Alternating Binary String",
+      link: "https://leetcode.com/problems/minimum-changes-to-make-alternating-binary-string/",
+      category: "Arrays & Hashing",
+      difficulty: "Easy",
+      solution: "minimum-changes-to-make-alternating-binary-string.py",
+      dailyChallenge: new Date("2023-12-24"),
+    },
   ];
 
 export default tableData;
