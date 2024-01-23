ee91531dc915f13e256cea441c799d064bcd7a05
hengyulee <hengyuli901229@gmail.com>
2024-01-23 12:38:04 +0800

    refactor + 0123

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -4,24 +4,27 @@ import {
   InputLeftElement,
   InputRightElement,
 } from "@chakra-ui/react";
-import { useContext } from "react";
+import { FormEvent, useContext } from "react";
 import { BsSearch } from "react-icons/bs";
 import { RxCross1 } from "react-icons/rx";
+import { redirect } from "react-router-dom";
 import InputRefContexts from "../contexts/inputRefContexts";
 import useQuestionFilterStore from "../stores/QuestionFilterStore";
 
 const SearchInput = () => {
   const setSearchText = useQuestionFilterStore((s) => s.setSearchText);
+  // const [input, setInput] = useState('');
 
   const { inputRef: ref } = useContext(InputRefContexts);
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!ref.current?.value) return;
+    setSearchText(ref.current.value);
+    // redirect(`/?search=${ref.current.value}`); // needs setting up router provider
+  }
 
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        if (ref.current) setSearchText(ref.current.value);
-      }}
-    >
+    <form onSubmit={handleSubmit}>
       <InputGroup>
         <InputLeftElement children={<BsSearch />} pointerEvents="none" />
         <Input
--- src/stores/QuestionFilterStore.ts
+++ src/stores/QuestionFilterStore.ts
@@ -1,33 +1,41 @@
 import { create } from "zustand";
 
+export type Difficulty = 'Easy' | 'Medium' | 'Hard';
+
 export interface Filters {
-    text?: string;
-    category?: string;
-    difficulty?: string;
-    onlyDC?: boolean;
+    text: string;
+    category: string;
+    difficulty: Difficulty;
+    onlyDC: boolean;
 }
 
 interface QuestionFilterStore {
     filters: Filters;
     setCategory: (category: string) => void;
-    setDifficulty: (difficulty: string) => void;
+    setDifficulty: (difficulty: Difficulty) => void;
     setSearchText: (text: string) => void;
     setDC: (onlyDC: boolean) => void;
 }
 
 const useQuestionFilterStore = create<QuestionFilterStore>((set) => ({
-    filters: {},
+    filters: {
+        text: "",
+        category: "Arrays & Hashing",
+        difficulty: "Easy",
+        onlyDC: false
+    }, // default values
+
     setCategory: (category: string) => {
         set(store => ({ filters: { ...store.filters, category } }));
     },
-    setDifficulty: (difficulty: string) => {
-        set(store => ({ filters: {...store.filters, difficulty} }));
+    setDifficulty: (difficulty: Difficulty) => {
+        set(store => ({ filters: { ...store.filters, difficulty } }));
     },
     setSearchText: (text: string) => {
-        set(store => ({ filters: {...store.filters, text} }));
+        set(store => ({ filters: { ...store.filters, text } }));
     },
     setDC: (onlyDC: boolean) => {
-        set(store => ({ filters: {...store.filters, onlyDC} }));
+        set(store => ({ filters: { ...store.filters, onlyDC } }));
     }
 }));
 
