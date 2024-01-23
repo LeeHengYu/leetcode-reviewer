66dcade2f63c93047549d0db1665c4d778a7b9d1
hengyulee <hengyuli901229@gmail.com>
2024-01-23 12:58:44 +0800

    fix date comparator

--- src/hook/dataSorting.ts
+++ src/hook/dataSorting.ts
@@ -4,43 +4,41 @@ const dataSorting = (data: Question[], reverse: boolean, by?: string) => {
     const mapping = ["Easy", "Medium", "Hard"];
     let sortedData = data;
 
-    if (!by) 
+    if (!by)
         by = "date";
 
-    if (by==="category"){
-        sortedData = data.sort((a,b) => {
+    if (by === "category") {
+        sortedData = data.sort((a, b) => {
             //sort by category in lexical order
             return a.category.localeCompare(b.category);
         })
     }
 
-    else if (by==="difficulty"){
-        sortedData = data.sort((a,b) => {
+    else if (by === "difficulty") {
+        sortedData = data.sort((a, b) => {
             return mapping.indexOf(a.difficulty) - mapping.indexOf(b.difficulty);
         })
     }
 
-    else if (by==="date"){
-        sortedData = data.sort((a,b) => {
-            if (a.dailyChallenge && !b.dailyChallenge) return -1;
-            if (!a.dailyChallenge && b.dailyChallenge) return 1;
+    else if (by === "date") {
+        sortedData = data.sort((a, b) => {
             if (a.dailyChallenge && b.dailyChallenge) {
-                const date1 = new Date(a.dailyChallenge).getTime();
-                const date2 = new Date(b.dailyChallenge).getTime();
-                return date2 - date1;
+                return b.dailyChallenge.valueOf() - a.dailyChallenge.valueOf();
             }
+            if (a.dailyChallenge) return -1;
+            if (b.dailyChallenge) return 1;
             return 0;
         })
     }
 
     else {
-        sortedData = data.sort((a,b) => {
+        sortedData = data.sort((a, b) => {
             return a.question.localeCompare(b.question);
         })
     }
 
     if (reverse) return sortedData.reverse();
-    return sortedData; 
+    return sortedData;
 }
 
 export default dataSorting;
